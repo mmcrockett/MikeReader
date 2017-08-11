572badd41e710a545b549ffe7b4f2cb382f28b91
Mike Crockett <github@mmcrockett.com>
2017-08-11 14:40:06 -0500

    Fixing secrets generation.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_MikeReader_session'
+Rails.application.config.session_store :cookie_store, key: "#{Rails.env}_mikereader_session"

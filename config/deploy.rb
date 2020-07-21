722ceaff034b7651c6bd983e0a74af11a86e1447
Mike Crockett <github@mmcrockett.com>
2020-07-21 10:13:56 -0500

    Adding staging db.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,12 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock '~> 3.14.1'
 
 set :application, 'mikereader'
 set :repo_url, 'git@github.com:mmcrockett/MikeReader.git'
 
-append :linked_dirs, "log"
-append :linked_dirs, "tmp"
+append :linked_dirs, 'log'
+append :linked_dirs, 'tmp'
+append :linked_files, 'config/credentials/staging.key'
 
 namespace :deploy do
   desc 'Restart application'

4c75c95836934fe385db0762c2f273bb32e324fe
Mike Crockett <github@mmcrockett.com>
2020-07-21 19:27:15 -0500

    Fixing staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,12 +3,12 @@ set :deploy_to, File.join('', 'home', USER, 'reader.test.mmcrockett.com')
 append :linked_files, 'config/credentials/staging.key'
 
 namespace :deploy do
-  desc 'Setup passenger in htaccess'
-  task :htaccess_config do
+  desc 'Setup passenger rails env to staging'
+  task :htaccess_rails_env do
     on roles(:app), in: :sequence, wait: 5 do
       execute :echo, 'SetEnv RAILS_ENV staging', '>>', release_path.join('.htaccess')
     end
   end
 
-  before :restart, :htaccess_config
+  before :restart, :htaccess_rails_env
 end

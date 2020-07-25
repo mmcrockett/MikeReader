7690f5fb3eca43244d0d4530b4d19201a99e95f6
Mike Crockett <github@mmcrockett.com>
2020-07-25 08:36:26 -0500

    UI tweaks for header color and buttons.

--- app/javascript/components/Navigation.jsx
+++ app/javascript/components/Navigation.jsx
@@ -3,9 +3,19 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 function Navigation(props) {
+  const getClassNames = () => {
+    if (process.env.production) {
+      return 'primary';
+    } else if (process.env.staging) {
+      return 'secondary';
+    } else {
+      return 'warning';
+    }
+  };
+
   return (
     <React.Fragment>
-      <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar bg={getClassNames()} variant={'dark'} expand={'lg'}>
         <Navbar.Brand href="/entries">Mike Reader</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/feeds">Feeds</Nav.Link>

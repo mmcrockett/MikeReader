2ad67b555de051eadd9fbca1560e605bcf402867
Mike Crockett <github@mmcrockett.com>
2020-07-25 13:27:36 -0500

    More logging.

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -11,7 +11,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   React.useEffect(() => {
-    console.warn('Node Env: ' + process.env.NODE_ENV);
+    console.warn('Node Env:  ' + process.env.NODE_ENV);
+    console.warn('Rails Env: ' + process.env.RAILS_ENV);
   }, []);
 
   const [messages, setMessages] = React.useState({});
